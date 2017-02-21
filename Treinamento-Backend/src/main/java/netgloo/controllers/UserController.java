eeab4a9b5b0594862545de39209220248fa9d2d8
estagiocit <estagiocit@cit>
2017-02-21 14:40:59 -0300

    commit user

--- Treinamento-Backend/src/main/java/netgloo/controllers/UserController.java
+++ Treinamento-Backend/src/main/java/netgloo/controllers/UserController.java
@@ -1,40 +1,57 @@
 package netgloo.controllers;
 
+import netgloo.Excecoes.UserInvalidoException;
+import netgloo.Excecoes.UserNaoCadastradoException;
+import netgloo.Service.UserService;
 import netgloo.models.User;
-import netgloo.models.UserDao;
 
+import netgloo.models.UserSearchForm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.xml.ws.soap.MTOM;
+import java.util.List;
 
 /**
  * Class UserController
  */
 @Controller
+@RequestMapping(value="/usuario")
 public class UserController {
+  // ------------------------
+  // PRIVATE FIELDS
+  // ------------------------
+
+  // Wire the UserDao used inside this controller.
+  @Autowired
+  private UserService userService;
+
 
   // ------------------------
   // PUBLIC METHODS
   // ------------------------
 
   /**
-   * Create a new user with an auto-generated id and email and name as passed 
+   * Create a new user with an auto-generated id and email and name as passed
    * values.
    */
   @RequestMapping(value="/create")
   @ResponseBody
-  public String create(String email, String name) {
+  public String create(@RequestBody User user) {
     try {
-      User user = new User(email, name);
-      userDao.create(user);
-  }
-    catch (Exception ex) {
-      return "Error creating the user: " + ex.toString();
+
+      userService.create(user);
+  } catch (UserInvalidoException e) {
+      return e.getMessage();
     }
+
     return "User succesfully created!";
   }
-  
+
   /**
    * Delete the user with the passed id.
    */
@@ -42,56 +59,43 @@ public class UserController {
   @ResponseBody
   public String delete(long id) {
     try {
-      User user = new User(id);
-      userDao.delete(user);
+
+      if(userService.delete(id)){
+        return "OK";
+      }else{
+        return "Errou!Usuário não existe";
+      }
+    } catch (UserNaoCadastradoException e) {
+      return e.getMessage();
     }
-    catch (Exception ex) {
-      return "Error deleting the user: " + ex.toString();
-    }
-    return "User succesfully deleted!";
+
+    //return "User succesfully deleted!";
   }
-  
-  /**
-   * Retrieve the id for the user with the passed email address.
-   */
-  @RequestMapping(value="/get-by-email")
-  @ResponseBody
-  public String getByEmail(String email) {
-    String userId;
-    try {
-      User user = userDao.getByEmail(email);
-      userId = String.valueOf(user.getId());
-    }
-    catch (Exception ex) {
-      return "User not found: " + ex.toString();
-    }
-    return "The user id is: " + userId;
-  }
-  
+
+
   /**
    * Update the email and the name for the user indentified by the passed id.
    */
   @RequestMapping(value="/update")
   @ResponseBody
-  public String updateName(long id, String email, String name) {
+  public String update(@RequestBody User user) {
     try {
-      User user = userDao.getById(id);
-      user.setEmail(email);
-      user.setName(name);
-      userDao.update(user);
+      boolean retorno = userService.update(user);
+      if (!retorno) {
+        return "Erro na criação do usuário";
+      }
+    } catch (UserNaoCadastradoException e) {
+      return e.getMessage();
     }
-    catch (Exception ex) {
-      return "Error updating the user: " + ex.toString();
-    }
-    return "User succesfully updated!";
-  } 
 
-  // ------------------------
-  // PRIVATE FIELDS
-  // ------------------------
-  
-  // Wire the UserDao used inside this controller.
-  @Autowired
-  private UserDao userDao;
-  
+    return "User succesfully updated!";
+  }
+
+  @RequestMapping(value="/search-user")
+  @ResponseBody
+  public List<User> search(@RequestBody UserSearchForm userSearchForm) {
+    return userService.search(userSearchForm);
+  }
+
+
 } // class UserController
--- Treinamento-Backend/src/main/java/netgloo/models/User.java
+++ Treinamento-Backend/src/main/java/netgloo/models/User.java
@@ -6,6 +6,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
+import java.util.Date;
 
 /**
  * Represents an User for this web application.
@@ -27,6 +28,14 @@ public class User {
   
   @NotNull
   private String name;
+
+  @NotNull
+  private String login;
+
+  @NotNull
+  private String senha;
+
+  private Date ultimoAcesso;
 
   // ------------------------
   // PUBLIC METHODS
@@ -66,5 +75,39 @@ public class User {
   public void setName(String value) {
     this.name = value;
   }
-  
+
+  public String getSenha() {
+    return senha;
+  }
+
+  public String getLogin() {
+    return login;
+  }
+
+  public void setLogin(String login) {
+    this.login = login;
+  }
+
+  public void setSenha(String senha) {
+    this.senha = senha;
+  }
+
+  public Date getUltimoAcesso() {
+    return ultimoAcesso;
+  }
+
+  public void setUltimoAcesso(Date ultimoAcesso) {
+    this.ultimoAcesso = ultimoAcesso;
+  }
+
+  public boolean isValid(){
+    if(this.email != null && !this.email.isEmpty() &&
+            this.name != null && !this.name.isEmpty() &&
+            this.login != null && !this.login.isEmpty() &&
+            this.senha != null && !this.senha.isEmpty() && this.senha.length() > 5){
+
+        return true;
+    }
+    return false;
+  }
 } // class User
